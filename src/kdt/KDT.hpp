ef42eb73132fcb594d944b4055a6f32809459172
nquotidian <qiniu@eng.ucsd.edu>
2019-10-15 18:33:51 +0000

    Add build() func

--- src/kdt/KDT.hpp
+++ src/kdt/KDT.hpp
@@ -53,10 +53,23 @@ class KDT {
           iheight(-1) {}
 
     /** Destructor of KD tree */
-    virtual ~KDT() { deleteAll(root); }
+    virtual ~KDT() {
+        deleteAll(root);
+        iheight = -1;
+        isize = 0;
+    }
 
-    /** TODO */
-    void build(vector<Point>& points) {}
+    /** build kd tree
+     *  if(points.empty())
+     *    return;
+     *  root = buildSubtree(points, 0, points.size()-1, 1, -1)
+     *
+     */
+    void build(vector<Point>& points) {
+        if (points.empty()) return;
+        // initial call when building a kd tree
+        root = buildSubtree(points, 0, points.size() - 1, 0, iheight);
+    }
 
     /** TODO */
     Point* findNearestNeighbor(Point& queryPoint) { return nullptr; }
@@ -66,17 +79,61 @@ class KDT {
         return {};
     }
 
-    /** TODO */
-    unsigned int size() const { return 0; }
+    /** Return the size of the KD tree */
+    unsigned int size() const { return isize; }
 
-    /** TODO */
-    int height() const { return 0; }
+    /** Return the height of the KD tree */
+    int height() const { return iheight; }
 
   private:
-    /** TODO */
+    /** TODO
+     *  Recursively
+     *  ~ build() method call buildSubtree
+     *  points:  vector with all the data points to build the KD tree
+     *  start: the inclusive start index of the points vector during building
+     * subtree end: the exclusive end index of the points vector during building
+     * subtree curDim: the current dimension during building subtree height: the
+     * current height during building subtree
+     *
+     *  pusedocode:
+     *  int medi;
+     *  KDNode* node = nullptr;
+     *  if(start<=end)
+     *      sort(start, end, curDim)      // sort according to the current
+     * dimension medi = floor((start+end)/2);  // set the medi to the middle
+     * node node = new KDNode(points[medi]); isize++; node.left =
+     * buildSubtree(points, start, medi-1, curDim+1, height+1) node.right =
+     * buildSubtree(points, medi+1, end, curDim+1, height+1)
+     *
+     *      iheight++;
+     *      return node;
+     *  else
+     *      return nullptr;
+     */
     KDNode* buildSubtree(vector<Point>& points, unsigned int start,
                          unsigned int end, unsigned int curDim, int height) {
-        return nullptr;
+        int medi = 0;
+        numDim = points.begin()->numDim;
+
+        if (start <= end) {
+            // Sorts the elements between [points.begin() + start,
+            // points.begin() + end + 1) according to it's dimension
+            sort(points.begin() + start, points.begin() + end + 1,
+                 CompareValueAt(curDim));
+            // calculate the parent node of subtree
+            medi = floor((start + end) / 2);
+            // node = new KDNode(points[medi]);
+            KDNode* node = new KDNode(points[medi]);
+            isize++;
+            node->right = buildSubtree(points, medi + 1, end,
+                                       (curDim + 1) % numDim, height + 1);
+            node->left = buildSubtree(points, start, medi - 1,
+                                      (curDim + 1) % numDim, height + 1);
+            iheight++;
+            return node;
+        } else {
+            return nullptr;
+        }
     }
 
     /** TODO */
@@ -87,8 +144,13 @@ class KDT {
                            vector<pair<double, double>>& queryRegion,
                            unsigned int curDim) {}
 
-    /** TODO */
-    static void deleteAll(KDNode* n) {}
+    /** Helper method of destructor, recursively delete the tree */
+    static void deleteAll(KDNode* n) {
+        if (n) {
+            deleteAll(n->left);
+            deleteAll(n->right);
+        }
+    }
 
     // Add your own helper methods here
 };
